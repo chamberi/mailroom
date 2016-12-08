94fd8a7cbe5a4c44f5debac77a09a9cf7aad768c
Colin Lamont <colinlamont@gmail.com>
2016-12-08 13:13:32 -0800

    adding completed setup files.

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""The setup for my donation_manager."""
+
+from setuptools import setup
+
+setup(
+    name="donation_manager",
+    description="An implmentation of the donation manager.",
+    version=0.1,
+    author="Colin Lamont",
+    author_email="colinlamont@gmail.com",
+    license="MIT",
+    py_modules=['donation_manager'],
+    package_dir={'': 'src'},
+    install_requires=['tox'],
+    extras_require={
+        "test": ["pytest", "pytest-watch", "pytest-cov"]
+    },
+)
