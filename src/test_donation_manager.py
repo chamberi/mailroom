7abdd0b614806c0a6ab13f8ff764a8dbb35457fd
Colin Lamont <colinlamont@gmail.com>
2016-12-08 15:01:31 -0800

    test when user inputs a new name not in dictionary

--- src/test_donation_manager.py
+++ src/test_donation_manager.py
@@ -1,20 +1,13 @@
 """This is test module for donation_manager."""
 import pytest
 
-THANKS_TABLE = [
-    ["q", "quitting"],
-    ["Q", "quitting"],
-    ["quit", "quitting"],
-    ["QUIT", "quitting"],
-    ["Quit", "quitting"],
-    ["list", "printing list"],
-    ["LIST", "printing list"],
-    ["List", "printing list"],
+LIST_TABLE = [
+    ["sally jones", {"john smith": [10, 20, 30]}, {"john smith": [10, 20, 30], "sally jones": []}],
 ]
 
 
-@pytest.mark.parametrize("response, result", THANKS_TABLE)
-def test_send_thanks(response, result):
-    """Test send thanks."""
+@pytest.mark.parametrize("user_input, dictionary, result", LIST_TABLE)
+def test_send_thanks_new_name(user_input, dictionary, result):
+    """Test send thanks when user adds a new name."""
     from donation_manager import send_thanks
-    assert send_thanks(response) == result
+    assert send_thanks(user_input, dictionary) == result
